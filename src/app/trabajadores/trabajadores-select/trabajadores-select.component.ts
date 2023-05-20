e86ed7a56b567eb70878a38dae7d3c377b5c60eb
KevinF13 <kevinfajardo110@gmail.com>
2023-05-19 20:06:10 -0500

    ConsumoApiTrabajador

--- src/app/trabajadores/trabajadores-select/trabajadores-select.component.ts
+++ src/app/trabajadores/trabajadores-select/trabajadores-select.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./trabajadores-select.component.css']
 })
 export class TrabajadoresSelectComponent {
-
+  trabajadores: any[] = [];
+  datosTablaOriginal: any[] = [];
+  currentPage = 1;
+  itemsPerPage = 10;
+  
+  constructor(private http: HttpClient) {} // Inyecta HttpClient en el constructor
+  ngOnInit(): void {
+    this.fetchTrabajadores()
+  }
+  fetchTrabajadores(): void {
+    const params = new HttpParams()
+      .set('page', this.currentPage.toString())
+      .set('itemsPerPage', this.itemsPerPage.toString());
+  
+    this.http.get<any[]>('/api/Api/trabajador/select?sucursal=2').subscribe(
+      data => {
+        this.trabajadores = data;
+        this.datosTablaOriginal = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
